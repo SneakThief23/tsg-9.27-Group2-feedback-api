9889e1611729ba7a7f06d812193ea7c492d86928
Abdulkader <aabdulakader@gmail.com>
2025-11-11 11:59:21 -0600

    Refactoring

--- src/main/java/com/tsg/feedbackapi/repositories/FeedbackRepo.java
+++ src/main/java/com/tsg/feedbackapi/repositories/FeedbackRepo.java
@@ -3,9 +3,10 @@ package com.tsg.feedbackapi.repositories;
 import com.tsg.feedbackapi.repositories.entities.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FeedbackRepo extends JpaRepository <Feedback, UUID> {
-    Optional<Feedback> findByMemberId(String memberId);
+    List<Feedback> findByMemberId(String memberId);
 }
